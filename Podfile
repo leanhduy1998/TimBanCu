a7ef267782c53bb29564c3aec27ad9dfeae8dbcb
Duy Le <leanhduy1998@gmail.com>
2018-06-29 14:36:02 -0400

    finish tieuhoc

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TimBanCu' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TimBanCu
+
+
+  pod 'ImageSlideshow'
+
+
+
+  target 'TimBanCuTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TimBanCuUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- TimBanCu/ViewController.swift
+++ TimBanCu/ViewController.swift
@@ -13,6 +13,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let path = Bundle.main.path(forResource: "tieuhoctphcm", ofType: "txt") {
+            do {
+                let data = try String(contentsOfFile: path, encoding: .utf8)
+                let myStrings = data.components(separatedBy: .newlines)
+                
+               /* for str in myStrings{
+                    if(!str.isEmpty){
+                        print(str)
+                    }
+                }*/
+                
+                var count = 0
+                while(count<myStrings.count){
+                    
+                    
+                    count = count + 2
+                    
+                    if(count<myStrings.count){
+                        var name = myStrings[count]
+                        
+                        count = count + 2
+                        
+                        if(count<myStrings.count){
+                            let address = myStrings[count]
+                            //count = count + 6
+                            print(name)
+                            print(address)
+                            
+                            while(count<myStrings.count && myStrings[count] != "<tr>"){
+                                count = count + 1
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
