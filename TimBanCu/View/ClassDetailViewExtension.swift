9378503ae009febd74055dfeeb4db81ca14d4bad
Vy Le <lethaovy2001@gmail.com>
2018-07-15 20:22:24 -0700

    fix noResultLabel constraint in ClassDetailViewExtension

--- TimBanCu/View/ClassDetailViewExtension.swift
+++ TimBanCu/View/ClassDetailViewExtension.swift
@@ -10,27 +10,31 @@ import Foundation
 import UIKit
 
 extension ClassDetailViewController{
+    
     func setupNoResultLabelAndButton(topViewY:CGFloat, topViewHeight:CGFloat){
-        
-        let y = topViewY + topViewHeight + 40
-        
-        noResultLabel.frame = CGRect(x: 0, y: y, width: view.frame.width, height: 80)
-        noResultLabel.text = "Chưa có lớp. Bạn có muốn thêm lớp? Ví dụ: 10A11"
-        noResultLabel.numberOfLines = 2
-        
-        noResultAddNewClassBtn.frame = CGRect(x: 0, y: y + 80, width: view.frame.width, height: 40)
-        noResultAddNewClassBtn.setTitle("Thêm Lớp Mới", for: .normal)
-        noResultAddNewClassBtn.setTitleColor(UIColor.blue, for: .normal)
-        
-        noResultAddNewClassBtn.addTarget(self, action: #selector(self.addNewClassDetailBtnPressed(_:)), for: .touchUpInside)
-        
         view.addSubview(noResultLabel)
         view.addSubview(noResultAddNewClassBtn)
         
         view.bringSubview(toFront: noResultLabel)
         view.bringSubview(toFront: noResultAddNewClassBtn)
         
-        view.layoutIfNeeded()
+        noResultLabel.text = "Chưa có lớp. Bạn có muốn thêm lớp? Ví dụ: 10A11"
+        noResultLabel.textColor = UIColor.darkGray
+        noResultLabel.textAlignment = .center
+        noResultLabel.numberOfLines = 2
+        noResultLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        noResultLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        noResultLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        noResultLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        noResultLabel.rightAnchor.constraint(equalTo: view.leftAnchor, constant: -40).isActive = true
+        noResultLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        
+        let y = topViewY + topViewHeight + 40
+        noResultAddNewClassBtn.frame = CGRect(x: 0, y: y + 80, width: view.frame.width, height: 40)
+        noResultAddNewClassBtn.setAttributedTitle(NSAttributedString(string: "Thêm Lớp Mới", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.semibold), NSAttributedStringKey.foregroundColor: UIColor(red: 255/255, green: 158/255, blue: 0/255, alpha: 1.0)]), for: .normal)
+        
+        noResultAddNewClassBtn.addTarget(self, action: #selector(self.addNewClassDetailBtnPressed(_:)), for: .touchUpInside)
     }
     
     func updateTableviewVisibilityBasedOnSearchResult(){
