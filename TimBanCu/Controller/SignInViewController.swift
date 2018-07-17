f347aa8aabc2709f7d38d6d27bb1e6d742d7cdce
Vy Le <lethaovy2001@gmail.com>
2018-07-16 20:15:58 -0700

    Add shimmering effects on appNameLabel in SignInVC

--- TimBanCu/Controller/SignInViewController.swift
+++ TimBanCu/Controller/SignInViewController.swift
@@ -22,9 +22,55 @@ class SignInViewController: UIViewController,GIDSignInDelegate, GIDSignInUIDeleg
     var ref: DatabaseReference!
     
     @IBOutlet weak var googleSignInBtn: GIDSignInButton!
+    
+    let shimmerAppNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Tìm bạn cũ"
+        label.font = UIFont.systemFont(ofSize: 45, weight: .regular)
+        label.textColor = UIColor(red: 255/255, green: 158/255, blue: 0, alpha: 1.0)
+        label.textAlignment = .center
+        return label
+    }()
+
+    let appNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Tìm bạn cũ"
+        label.font = UIFont.systemFont(ofSize: 45, weight: .regular)
+        label.textColor = UIColor(red: 255/255, green: 158/255, blue: 0, alpha: 0.6)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    func setupShimmeringText() {
+        view.addSubview(appNameLabel)
+        view.addSubview(shimmerAppNameLabel)
+        
+        appNameLabel.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 400)
+        shimmerAppNameLabel.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 400)
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = appNameLabel.bounds
+        gradient.colors = [UIColor.clear.cgColor, UIColor.white.cgColor, UIColor.clear.cgColor]
+        gradient.locations = [0.0, 0.5, 1]
+        let angle = -60 * CGFloat.pi / 180
+        gradient.transform = CATransform3DMakeRotation(angle, 0, 0, 1)
+        
+        shimmerAppNameLabel.layer.mask = gradient
+        
+        let animation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.duration = 2
+        animation.repeatCount = Float.infinity
+        animation.autoreverses = false
+        animation.fromValue = -view.frame.width
+        animation.toValue = view.frame.width
+        gradient.add(animation, forKey: "shimmerKey")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupShimmeringText()
+        
         ref = Database.database().reference()
         
         GIDSignIn.sharedInstance().clientID = "137184194492-5iokn36749o7gmlodjnv6gskddjen7p1.apps.googleusercontent.com"
