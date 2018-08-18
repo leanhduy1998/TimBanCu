6d52c9a7b0da60ba0d5d92e77447f5ba00a90c38
Vy Le <lethaovy2001@gmail.com>
2018-08-18 12:58:11 -0700

    Save for merge

--- TimBanCu/Controller/SettingViewController.swift
+++ TimBanCu/Controller/SettingViewController.swift
@@ -21,6 +21,8 @@ class SettingViewController: UIViewController, UITableViewDelegate,UITableViewDa
     var settings = ["Sửa Thông Tin Cá Nhân","Đăng Xuất"]
     var icons = ["edit", "signOut"]
     
+    var finishedLoadingInitialTableCells = false
+    
     let customSelectionColorView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor(red: 255/255, green: 204/255, blue: 0, alpha: 0.2)
@@ -31,6 +33,10 @@ class SettingViewController: UIViewController, UITableViewDelegate,UITableViewDa
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        finishedLoadingInitialTableCells = false
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,6 +49,52 @@ class SettingViewController: UIViewController, UITableViewDelegate,UITableViewDa
         cell?.settingImage.image = UIImage(named: icons[indexPath.row])
         cell?.selectedBackgroundView? = customSelectionColorView
         return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        //1. Setup the CATransform3D structure
+        
+        var rotation = CATransform3D()
+        
+        rotation = CATransform3DMakeRotation((90.0 * .pi) / 180, 0.0, 0.7, 0.4)
+        
+        rotation.m34 = 1.0 / -600
+        
+        //2. Define the initial state (Before the animation)
+        
+        cell.layer.shadowColor = UIColor.black.cgColor
+        
+        cell.layer.shadowOffset = CGSize(width: CGFloat(10), height: CGFloat(10))
+        
+        cell.alpha = 0
+        
+        cell.layer.transform = rotation
+        
+        cell.layer.anchorPoint = CGPoint(x: CGFloat(0), y: CGFloat(0.5))
+        
+        //!!!FIX for issue #1 Cell position wrong————
+        
+        if cell.layer.position.x != 0 {
+            
+            cell.layer.position = CGPoint(x: CGFloat(0), y: CGFloat(cell.layer.position.y))
+            
+        }
+        
+        //4. Define the final state (After the animation) and commit the animation
+        
+        UIView.beginAnimations("rotation", context: nil)
+        
+        UIView.setAnimationDuration(0.8)
+        
+        cell.layer.transform = CATransform3DIdentity
+        
+        cell.alpha = 1
+        
+        cell.layer.shadowOffset = CGSize(width: CGFloat(0), height: CGFloat(0))
+        
+        UIView.commitAnimations()
+        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
